3c9634bc76f2b9abc06851bd3c9aa4515a5bd031
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2011-09-12 12:21:18 +0000

    [hector_trajectory_server] -fix wrong source file :)
    git-svn-id: https://tu-darmstadt-ros-pkg.googlecode.com/svn/trunk/hector_slam@46 362d0299-bcd8-93f0-de3f-00335e8b5ee8

--- hector_trajectory_server/src/hector_trajectory_server.cpp
+++ hector_trajectory_server/src/hector_trajectory_server.cpp
@@ -1,4 +1,3 @@
-
 //=================================================================================================
 // Copyright (c) 2011, Stefan Kohlbrecher, TU Darmstadt
 // All rights reserved.
@@ -30,15 +29,20 @@
 #include <cstdio>
 #include "ros/ros.h"
 #include "ros/console.h"
-#include "nav_msgs/GetMap.h"
+//#include "nav_msgs/GetMap.h"
+
+#include "nav_msgs/Path.h"
 #include "std_msgs/String.h"
+
 #include "geometry_msgs/Quaternion.h"
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
 
-#include <hector_map_tools/HectorMapTools.h>
+#include "tf/transform_listener.h"
+//#include "geometry_msgs/PoseWithCovarianceStamped.h"
 
-#include <hector_nav_msgs/GetRobotPath.h>
-#include <hector_nav_msgs/PoseList.h>
+//#include <hector_map_tools/HectorMapTools.h>
+
+#include <hector_nav_msgs/GetRobotTrajectory.h>
+//#include <hector_nav_msgs/PoseList.h>
 
 #include <tf/tf.h>
 
@@ -53,15 +57,27 @@ class PathContainer
 public:
   PathContainer()
   {
-    ros::NodeHandle n;
+    ros::NodeHandle private_nh("~");
 
-    sys_cmd_sub_ = n.subscribe("syscommand", 1, &PathContainer::sysCmdCallback, this);
+    private_nh.param("target_frame_name", p_target_frame_name_, std::string("map"));
+    private_nh.param("source_frame_name", p_source_frame_name_, std::string("scanmatcher_frame"));
+    private_nh.param("trajectory_update_rate", p_update_rate_, 4.0);
 
-    pose_update_sub_ = n.subscribe("poseupdate", 1, &PathContainer::poseUpdateCallback, this);
 
-    poses_provider_service_ = n.advertiseService("robot_path", &PathContainer::posesProviderCallBack, this);
+    sys_cmd_sub_ = private_nh.subscribe("syscommand", 1, &PathContainer::sysCmdCallback, this);
+
+    //pose_update_sub_ = n.subscribe("poseupdate", 1, &PathContainer::poseUpdateCallback, this);
+
+    trajectory_provider_service_ = private_nh.advertiseService("robot_trajectory", &PathContainer::trajectoryProviderCallBack, this);
 
     last_reset_time_ = ros::Time::now();
+
+    update_timer_ = private_nh.createTimer(ros::Duration(1.0 / p_update_rate_), &PathContainer::trajectoryUpdateTimerCallback, this, false);
+
+    pose_source_.pose.orientation.w = 1.0;
+    pose_source_.header.frame_id = p_source_frame_name_;
+
+    trajectory_.trajectory.header.frame_id = p_target_frame_name_;
 
     //dist_lookup_service_ = nh.advertiseService(lookup_service_name, &OccupancyGridContainer::lookupServiceCallback, this);
   }
@@ -70,10 +86,39 @@ public:
   {
     if (sys_cmd.data == "reset"){
       last_reset_time_ = ros::Time::now();
-      path_.poses.poses.clear();
+      //path_.poses.poses.clear();
     }
   }
 
+  void trajectoryUpdateTimerCallback(const ros::TimerEvent& event)
+  {
+    //ROS_INFO("blaa");
+
+    try{
+      pose_source_.header.stamp = ros::Time::now();
+
+      //tf::StampedTransform transform;
+      geometry_msgs::PoseStamped pose_out;
+
+      //tf_.lookupTransform(p_target_frame_name_,p_source_frame_name_,ros::Time(0), transform);
+      ros::Duration dur (0.5);
+      tf_.waitForTransform(p_target_frame_name_, pose_source_.header.frame_id, pose_source_.header.stamp, dur);
+      tf_.transformPose(p_target_frame_name_, pose_source_, pose_out);
+
+      //tf::poseTFToMsg(transform, geom_pose.pose);
+
+
+      trajectory_.trajectory.poses.push_back(pose_out);
+      trajectory_.trajectory.header.stamp = pose_source_.header.stamp;
+      //trajectory_.GetRobotTrajectoryResponse_.trajectory.poses.push_back(transform);
+    }
+    catch(tf::TransformException e)
+    {      
+      ROS_ERROR("Transform from %s to %s failed: %s \n", p_target_frame_name_.c_str(), pose_source_.header.frame_id.c_str(), e.what() );
+    }
+  }
+
+  /*
   void poseUpdateCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& pose_in)
   {
 
@@ -84,7 +129,7 @@ public:
     if ((now - last_reset_time_).toSec() > 0.5){
 
       //Wait some time so not too many redundant poses are recorded
-      if ( (now - last_pose_save_time_).toSec() > 0.5){
+      if ( (now - last_pose_save_time_).toSec() > 0.05){
         last_pose_save_time_ = now;
         geometry_msgs::Point32 new_pose;
 
@@ -105,26 +150,38 @@ public:
 
         pose_vector.push_back(new_pose);
       }
-    }
   }
+  */
 
-  bool posesProviderCallBack(hector_nav_msgs::GetRobotPath::Request  &req,
-                             hector_nav_msgs::GetRobotPath::Response &res )
+  bool trajectoryProviderCallBack(hector_nav_msgs::GetRobotTrajectory::Request  &req,
+                                  hector_nav_msgs::GetRobotTrajectory::Response &res )
   {
     ROS_INFO("hector_path_provider service called");
 
-    res = path_;
+    res = trajectory_;
 
     return true;
   };
 
-  ros::ServiceServer poses_provider_service_;
+  //parameters
+  std::string p_target_frame_name_;
+  std::string p_source_frame_name_;
+  double p_update_rate_;
+
+  // Zero pose used for transformation to target_frame.
+  geometry_msgs::PoseStamped pose_source_;
+
+  ros::ServiceServer trajectory_provider_service_;
+
+  ros::Timer update_timer_;
 
 
-  ros::Subscriber pose_update_sub_;
+  //ros::Subscriber pose_update_sub_;
   ros::Subscriber sys_cmd_sub_;
 
-  hector_nav_msgs::GetRobotPath::Response path_;
+  hector_nav_msgs::GetRobotTrajectory::Response trajectory_;
+
+  tf::TransformListener tf_;
 
   ros::Time last_reset_time_;
   ros::Time last_pose_save_time_;
@@ -134,14 +191,10 @@ public:
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "hector_path_saver");
-  std::string path_topic = "/poseupdate";
+  ros::init(argc, argv, "hector_trajectory_server");
+  //std::string path_name = "/poseupdate";
 
   PathContainer pc;
-
-  //GeotiffWriter geotiff_writer;
-  //geotiff_writer.setMapName("test");
-
 
   ros::spin();
 
