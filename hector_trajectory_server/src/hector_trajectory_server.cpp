1faff5f1bbbb118d97d58dbde62e1f90469d2b4c
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2011-09-14 08:48:16 +0000

    git-svn-id: https://tu-darmstadt-ros-pkg.googlecode.com/svn/trunk/hector_slam@52 362d0299-bcd8-93f0-de3f-00335e8b5ee8

--- hector_trajectory_server/src/hector_trajectory_server.cpp
+++ hector_trajectory_server/src/hector_trajectory_server.cpp
@@ -29,7 +29,6 @@
 #include <cstdio>
 #include "ros/ros.h"
 #include "ros/console.h"
-//#include "nav_msgs/GetMap.h"
 
 #include "nav_msgs/Path.h"
 #include "std_msgs/String.h"
@@ -37,12 +36,8 @@
 #include "geometry_msgs/Quaternion.h"
 
 #include "tf/transform_listener.h"
-//#include "geometry_msgs/PoseWithCovarianceStamped.h"
-
-//#include <hector_map_tools/HectorMapTools.h>
 
 #include <hector_nav_msgs/GetRobotTrajectory.h>
-//#include <hector_nav_msgs/PoseList.h>
 
 #include <tf/tf.h>
 
@@ -79,39 +74,30 @@ public:
     pose_source_.header.frame_id = p_source_frame_name_;
 
     trajectory_.trajectory.header.frame_id = p_target_frame_name_;
-
-    //dist_lookup_service_ = nh.advertiseService(lookup_service_name, &OccupancyGridContainer::lookupServiceCallback, this);
   }
 
   void sysCmdCallback(const std_msgs::String& sys_cmd)
   {
     if (sys_cmd.data == "reset"){
       last_reset_time_ = ros::Time::now();
-      //path_.poses.poses.clear();
+      trajectory_.trajectory.poses.clear();
+      trajectory_.trajectory.header.stamp = ros::Time::now();
     }
   }
 
   void trajectoryUpdateTimerCallback(const ros::TimerEvent& event)
   {
-    //ROS_INFO("blaa");
-
     try{
       pose_source_.header.stamp = ros::Time::now();
 
-      //tf::StampedTransform transform;
       geometry_msgs::PoseStamped pose_out;
 
-      //tf_.lookupTransform(p_target_frame_name_,p_source_frame_name_,ros::Time(0), transform);
       ros::Duration dur (0.5);
       tf_.waitForTransform(p_target_frame_name_, pose_source_.header.frame_id, pose_source_.header.stamp, dur);
       tf_.transformPose(p_target_frame_name_, pose_source_, pose_out);
 
-      //tf::poseTFToMsg(transform, geom_pose.pose);
-
-
       trajectory_.trajectory.poses.push_back(pose_out);
       trajectory_.trajectory.header.stamp = pose_source_.header.stamp;
-      //trajectory_.GetRobotTrajectoryResponse_.trajectory.poses.push_back(transform);
     }
     catch(tf::TransformException e)
     {      
@@ -124,46 +110,9 @@ public:
     trajectory_pub_.publish(trajectory_.trajectory);
   }
 
-  /*
-  void poseUpdateCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& pose_in)
-  {
-
-
-    ros::Time now = ros::Time::now();
-
-    //Wait for after a reset till starting to record data.
-    if ((now - last_reset_time_).toSec() > 0.5){
-
-      //Wait some time so not too many redundant poses are recorded
-      if ( (now - last_pose_save_time_).toSec() > 0.05){
-        last_pose_save_time_ = now;
-        geometry_msgs::Point32 new_pose;
-
-        new_pose.x = pose_in->pose.pose.position.x;
-        new_pose.y = pose_in->pose.pose.position.y;
-
-        tf::Transform transform;
-        tf::poseMsgToTF(pose_in->pose.pose,transform);
-
-
-        btScalar yaw, pitch, roll;
-        transform.getBasis().getEulerYPR(yaw, pitch, roll);
-
-        new_pose.z = yaw;
-
-        hector_nav_msgs::PoseList& pose_list (path_.poses);
-        std::vector<geometry_msgs::Point32>& pose_vector (pose_list.poses);
-
-        pose_vector.push_back(new_pose);
-      }
-  }
-  */
-
   bool trajectoryProviderCallBack(hector_nav_msgs::GetRobotTrajectory::Request  &req,
                                   hector_nav_msgs::GetRobotTrajectory::Response &res )
   {
-    //ROS_INFO("hector_path_provider service called");
-
     res = trajectory_;
 
     return true;
@@ -201,7 +150,6 @@ public:
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "hector_trajectory_server");
-  //std::string path_name = "/poseupdate";
 
   PathContainer pc;
 
