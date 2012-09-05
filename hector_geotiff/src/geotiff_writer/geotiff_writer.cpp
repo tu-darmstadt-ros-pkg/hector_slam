fb85c4580400e187f04c461e8099f35977a2b053
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2012-09-05 17:02:33 +0000

    merged differences between /trunk and /branches/fuerte
    git-svn-id: https://tu-darmstadt-ros-pkg.googlecode.com/svn/trunk/hector_slam@561 362d0299-bcd8-93f0-de3f-00335e8b5ee8

--- hector_geotiff/src/geotiff_writer/geotiff_writer.cpp
+++ hector_geotiff/src/geotiff_writer/geotiff_writer.cpp
@@ -26,6 +26,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //=================================================================================================
 
+#include <ros/console.h>
 #include "geotiff_writer/geotiff_writer.h"
 
 #include <QtGui/QPainter>
