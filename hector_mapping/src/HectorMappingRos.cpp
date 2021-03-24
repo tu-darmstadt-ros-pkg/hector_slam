134d9316f27d063d8ef89d2581772250057dc426
Stefan Fabian <github@stefanfabian.me>
2021-03-24 11:00:25 +0100

    No const reference for value type in cpp.

--- hector_mapping/src/HectorMappingRos.cpp
+++ hector_mapping/src/HectorMappingRos.cpp
@@ -591,7 +591,7 @@ void HectorMappingRos::initialPoseCallback(const geometry_msgs::PoseWithCovarian
   this->resetPose(msg->pose.pose);
 }
 
-void HectorMappingRos::toggleMappingPause(const bool &pause)
+void HectorMappingRos::toggleMappingPause(bool pause)
 {
   // Pause/unpause
   if (pause && !pause_scan_processing_)
